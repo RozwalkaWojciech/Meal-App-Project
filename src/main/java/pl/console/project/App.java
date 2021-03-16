ad40ab236f7d36765826373564c8c0a4c65eda27
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-16 22:22:09 +0100

    add Logger config and dependency

--- src/main/java/pl/console/project/App.java
+++ src/main/java/pl/console/project/App.java
@@ -1,7 +1,15 @@
 package pl.console.project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+
     public static void main(String[] args) {
+
+        STDOUT.info("Hello!");
 
     }
 }
