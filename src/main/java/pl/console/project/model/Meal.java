e5d8a5d36e1ab47376bef264025c384398d8c3c9
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-03-23 12:17:42 +0100

    Refactor, add List of measures

--- src/main/java/pl/console/project/model/Meal.java
+++ src/main/java/pl/console/project/model/Meal.java
@@ -16,13 +16,13 @@ public class Meal {
     private final String category;
     private final String instruction;
     private List<String> ingredients = new ArrayList<>();
-    //private List<String> measures = new ArrayList<>();
+    private List<String> measures = new ArrayList<>();
+
 
     @JsonAlias({"strIngredient1", "strIngredient2", "strIngredient3", "strIngredient4", "strIngredient5",
-            "strIngredient6", "strIngredient7", "strIngredient8", "strIngredient9", "strIngredient10", "strIngredient11",
-            "strIngredient12", "strIngredient13",
-            "strIngredient14", "strIngredient15", "strIngredient16",
-            "strIngredient17", "strIngredient18", "strIngredient19", "strIngredient20"})
+            "strIngredient6", "strIngredient7", "strIngredient8", "strIngredient9", "strIngredient10",
+            "strIngredient11", "strIngredient12", "strIngredient13", "strIngredient14", "strIngredient15",
+            "strIngredient16", "strIngredient17", "strIngredient18", "strIngredient19", "strIngredient20"})
     public String getFakeIngredient() {
         return null;
     }
@@ -31,6 +31,21 @@ public class Meal {
     public void setFakeIngredient(String ingredient) {
         if (ingredient != null) {
             ingredients.add(ingredient);
+        }
+    }
+
+    @JsonAlias({"strMeasure1", "strMeasure2", "strMeasure3", "strMeasure4", "strMeasure5",
+            "strMeasure6", "strMeasure7", "strMeasure8", "strMeasure9", "strMeasure10",
+            "strMeasure11", "strMeasure12", "strMeasure13", "strMeasure14", "strMeasure15",
+            "strMeasure16", "strMeasure17", "strMeasure18", "strMeasure19", "strMeasure20",})
+    public String getFakeMeasure() {
+        return null;
+    }
+
+    @JsonSetter()
+    public void setFakeMeasure(String measure) {
+        if (measure != null) {
+            measures.add(measure);
         }
     }
 
@@ -64,13 +79,18 @@ public class Meal {
         return ingredients;
     }
 
+    public List<String> getMeasures() {
+        return measures;
+    }
+
     @Override
     public String toString() {
         return "Meal{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
-                ", category='" + category + '\'' + '\'' +
-                ", ingredients=" + ingredients +
+                ", category='" + category + '\'' +
+                +'\'' + ", ingredients=" + ingredients +
+                ", measures=" + measures +
                 '}' + ", instruction='" + instruction + "\n";
     }
 }
