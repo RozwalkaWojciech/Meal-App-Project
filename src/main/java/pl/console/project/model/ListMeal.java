82217eaae96c7fe3aa64c8479da769e086afffc4
Maciej <maciejgrochowski93@o2.pl>
2021-03-25 15:09:04 +0100

    I have prepared some API methods.

--- src/main/java/pl/console/project/model/ListMeal.java
+++ src/main/java/pl/console/project/model/ListMeal.java
@@ -3,6 +3,7 @@ package pl.console.project.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -16,6 +17,43 @@ public class ListMeal {
 
     public List<Meal> getListMeals() {
         return listMeals;
+    }
+
+    public Meal findMealByName(String name) {
+        for (Meal meal : listMeals) {
+            if (meal.getName().equals(name)) {
+                return meal;
+            }
+        }
+        return null;
+    }
+
+    public List<Meal> findMealByCategory(String category) {
+        List<Meal> mealsCategoryList = new LinkedList<>();
+        for (Meal meal : listMeals) {
+            if (meal.getCategory().equals(category)) {
+                mealsCategoryList.add(meal);
+            }
+        }
+        return mealsCategoryList;
+    }
+
+    public List<Meal> findMealByIngredient(String ingredient) {
+        List<Meal> mealsIngredientList = new LinkedList<>();
+        for (Meal meal : listMeals) {
+            if (meal.getIngredients().contains(ingredient)) {
+                mealsIngredientList.add(meal);
+            }
+        }
+        return mealsIngredientList;
+    }
+
+    public void addMealToList(Meal meal) {
+        listMeals.add(meal);
+    }
+
+    public void removeMealFromList(Meal meal) {
+        listMeals.remove(meal);
     }
 
     @Override
