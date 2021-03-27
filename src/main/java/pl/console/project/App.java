5072fd76086667fa7671e93cd357ce668bb23989
Maciej Przekwas <ghostx99@gmail.com>
2021-03-27 22:47:42 +0100

    *created a method to launch the menu from the "App" class *displaying the assumed project graphics in the console *created a loop to continuously select options until finished *created a mechanism for colorizing the displayed text in the console

--- src/main/java/pl/console/project/App.java
+++ src/main/java/pl/console/project/App.java
@@ -2,14 +2,17 @@ package pl.console.project;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.console.project.menu.Menu;
 
 public class App {
 
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.method();
 
-        STDOUT.info("Hello!");
+        //STDOUT.info("Hello!");
 
     }
 }
--- src/main/java/pl/console/project/menu/Menu.java
+++ src/main/java/pl/console/project/menu/Menu.java
@@ -5,13 +5,14 @@ Main class in project to select choose on with option user wanna choose
 
 package pl.console.project.menu;
 
+import java.io.IOException;
 import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class Menu {
-
 
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
@@ -29,32 +30,43 @@ public class Menu {
     You could reference those as necessary.
     For example, using the above constants, you could make the following red text output on supported terminals:
     System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
-
      */
+    public void method() {
+        //STDOUT.info(ANSI_GREEN + "this text is green!" + ANSI_RESET);
 
-    public static void main(String[] args) {
-        System.out.println(ANSI_RED + "this text is red!" + ANSI_RESET);
+       // String leftAlignFormat = "| %-15s | %-4d |%n";
+        System.out.format(ANSI_WHITE + "+-----------------+------+%n");
+        System.out.format("| WELCOME TO             |%n");
+        System.out.format("| (®) Meal App Project   |%n");
+        System.out.format("+-----------------+------+%n");
+        System.out.format("| Authors:xyz            |%n");
+        System.out.format("+-----------------+------+%n");
+        System.out.format("+-----------------+------+%n" + ANSI_RESET);
 
-        STDOUT.info(ANSI_GREEN + "this text is green!" + ANSI_RESET);
+
+        System.out.println(ANSI_GREEN+"Choose from below choices"+ ANSI_RESET);
+        System.out.println("-------------------------\n");
+        System.out.println(ANSI_YELLOW+"1 - Manage data base Meal");
+        System.out.println("2 - Manage favourites Meal");
+        System.out.println("3 - Configuration");
+        System.out.println("9 - Quit"+ANSI_RESET);
+        System.out.println("-------------------------\n");
 
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1:
-                // Perform "original number" case.
-                break;
-            case 2:
-                // Perform "encrypt number" case.
-                break;
-            case 3:
-                // Perform "decrypt number" case.
-                break;
-            case 4:
-                // Perform "quit" case.
-                break;
-            default:
-                // The user input an unexpected choice.
+        int choice = 0;
+        do {
+            try {
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1 -> System.out.println("Manage data base Meal");
+                    case 2 -> System.out.println("Manage favourites Meal");
+                    case 3 -> System.out.println("3 - Configuration");
+                    case 9 -> System.out.println("End of the program");
+                    default -> System.out.println("please choose correct option");
+                }
+        } catch (Exception exc) {
+            System.out.println(ANSI_RED+"Please choose correct option"+ANSI_RESET);
         }
+        } while (choice != 9);
     }
 }
