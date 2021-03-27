59885f3c71048557cb0271d4e027d2eae7a8a6f2
Maciej Przekwas <ghostx99@gmail.com>
2021-03-28 00:03:54 +0100

    *change the input, from "scanner.nextInt()" to "scanner.nextLine()" *refactor the code

--- src/main/java/pl/console/project/menu/Menu.java
+++ src/main/java/pl/console/project/menu/Menu.java
@@ -5,13 +5,11 @@ Main class in project to select choose on with option user wanna choose
 
 package pl.console.project.menu;
 
-import java.io.IOException;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class Menu {
 
@@ -24,14 +22,12 @@ public class Menu {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
-
-    /*
+    /**
     You could reference those as necessary.
     For example, using the above constants, you could make the following red text output on supported terminals:
     System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
-     */
+     **/
     public void method() {
         //STDOUT.info(ANSI_GREEN + "this text is green!" + ANSI_RESET);
         // String leftAlignFormat = "| %-15s | %-4d |%n";
@@ -43,35 +39,34 @@ public class Menu {
         System.out.format("+-----------------+------+%n");
         System.out.format("+-----------------+------+%n" + ANSI_RESET);
 
-        System.out.println(ANSI_GREEN + "Choose from below choices" + ANSI_RESET);
+        //System.out.println(ANSI_GREEN + "Choose from below choices" + ANSI_RESET);
         System.out.println("-------------------------\n");
         System.out.println(ANSI_YELLOW + "1 - Manage data base Meal");
         System.out.println("2 - Manage favourites Meal");
         System.out.println("3 - Configuration");
         System.out.println("9 - Quit" + ANSI_RESET);
         System.out.println("-------------------------\n");
+        System.out.println(ANSI_CYAN+"Please choose an interesting option and press 'enter'"+ANSI_RESET);
 
         Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        while (choice != 9) {
+        String choice="";
+        while (!choice.equals("9")) {
 
             try {
-                choice = scanner.nextInt();
+                choice = scanner.nextLine();
                 switch (choice) {
-                    case 1 -> System.out.println("Manage data base Meal");
-                    case 2 -> System.out.println("Manage favourites Meal");
-                    case 3 -> System.out.println("3 - Configuration");
-                    case 9 -> System.out.println("End of the program");
-                    //default -> System.out.println("please choose correct option");
+                    case "1" -> System.out.println("Manage data base Meal");
+                    case "2" -> System.out.println("Manage favourites Meal");
+                    case "3" -> System.out.println("3 - Configuration");
+                    case "9" -> System.out.println("End of the program");
+                    default -> System.out.println(ANSI_RED+"please choose correct option"+ANSI_RESET);
                 }
 
             } catch (InputMismatchException ignored) {
                 System.out.println(ANSI_RED+"wrong parameter"+ ANSI_RESET);
-                break;
-                //TODO: check why the program falls into recursion without "break"
+                STDOUT.info(choice, " :parameter is ");
             }
         }
+        scanner.close();
     }
 }
-
-
