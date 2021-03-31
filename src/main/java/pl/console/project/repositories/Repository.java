53d425ab0584294d1790abf77916889cca69c23f
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-31 23:46:28 +0200

    refactor, add Optional

--- src/main/java/pl/console/project/repositories/Repository.java
+++ src/main/java/pl/console/project/repositories/Repository.java
@@ -7,12 +7,14 @@ import pl.console.project.model.Meal;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class Repository {
 
     private final ListMeal listMeal;
     private final ListMeal favoriteListMeal;
-    private static Repository INSTANCE;
+    private static Repository instance;
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final String DATA_BASE_PATH = "src/main/resources/meal list.json";
     private static final String USER_MEAL_LIST_PATH = "src/main/resources/user meal list.json";
     private static final String FAVORITE_MEAL_LIST_PATH = "src/main/resources/favorite meal list.json";
@@ -23,17 +25,17 @@ public class Repository {
     }
 
     public static Repository getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new Repository();
+        if (instance == null) {
+            instance = new Repository();
         }
-        return INSTANCE;
+        return instance;
     }
 
     public ListMeal getListMeal() {
         return listMeal;
     }
 
-    public Meal findMealByName(String name) {
+    public Optional<Meal> findMealByName(String name) {
         return listMeal.findMealByName(name);
     }
 
@@ -55,7 +57,7 @@ public class Repository {
         writeFile(USER_MEAL_LIST_PATH, listMeal);
     }
 
-    public Meal findFavMealByName(String name) {
+    public Optional<Meal> findFavMealByName(String name) {
         return favoriteListMeal.findMealByName(name);
     }
 
@@ -82,10 +84,9 @@ public class Repository {
     }
 
     public static ListMeal readFile(String path) {
-        ObjectMapper objectMapper = new ObjectMapper();
         ListMeal listMeal = null;
         try {
-            listMeal = objectMapper.readValue(new File(path), ListMeal.class);
+            listMeal = OBJECT_MAPPER.readValue(new File(path), ListMeal.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -93,19 +94,17 @@ public class Repository {
     }
 
     public static void writeFile(String path, ListMeal list) {
-        ObjectMapper objectMapper = new ObjectMapper();
         try {
-            objectMapper.writeValue(new File(path), list);
+            OBJECT_MAPPER.writeValue(new File(path), list);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void loadDataBase() {
-        ObjectMapper objectMapper = new ObjectMapper();
         ListMeal listMeal = readFile(DATA_BASE_PATH);
         try {
-            objectMapper.writeValue(new File(USER_MEAL_LIST_PATH), listMeal);
+            OBJECT_MAPPER.writeValue(new File(USER_MEAL_LIST_PATH), listMeal);
         } catch (IOException e) {
             e.printStackTrace();
         }
