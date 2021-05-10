a56b029b950362a9a3940d4d7bf387b7fdbee080
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-10 12:16:50 +0200

    add wrongChoice method, reorganize Menu, remove  static ANSI fields

--- src/main/java/pl/console/project/menu/Menu.java
+++ src/main/java/pl/console/project/menu/Menu.java
@@ -13,15 +13,6 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
     private Menu() {
@@ -33,16 +24,16 @@ public class Menu {
      * System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
      **/
     public static void mainMenu() {
-        //STDOUT.info(ANSI_GREEN + "this text is green!" + ANSI_RESET);
-        // String leftAlignFormat = "| %-15s | %-4d |%n";
-        //System.out.println(ANSI_GREEN + "Choose from below choices" + ANSI_RESET);
-        System.out.println("--------------------------");
-        System.out.println(ANSI_YELLOW + "1 - Manage data base Meal");
-        System.out.println("2 - Manage favourites Meal");
-        System.out.println("3 - Configuration");
-        System.out.println("9 - Quit" + ANSI_RESET);
-        System.out.println("--------------------------\n");
-        System.out.println(ANSI_CYAN + "Please choose an interesting option and press 'enter'" + ANSI_RESET);
+        STDOUT.info("\n────────────────────────────\n");
+        STDOUT.info("         MAIN MENU        \n");
+        STDOUT.info("────────────────────────────\n");
+        STDOUT.info(Color.YELLOW.get() + "1 - Search Meal\n");
+        STDOUT.info(Color.YELLOW.get() + "2 - Add/remove Meal\n");
+        STDOUT.info("3 - Manage favourites Meal\n");
+        STDOUT.info("4 - Configuration\n");
+        STDOUT.info("9 - Quit\n" + Color.RESET.get());
+        STDOUT.info("--------------------------\n");
+        STDOUT.info(Color.CYAN.get() + "Please choose an interesting option and press 'enter'\n" + Color.RESET.get());
 
         Scanner scanner = new Scanner(System.in);
         String choice = "";
@@ -50,17 +41,17 @@ public class Menu {
             try {
                 choice = scanner.nextLine();
                 switch (choice) {
-                    case "1" -> STDOUT.info("Manage data base Meal\n");
+                    case "1" -> SearchMenu.searchMenu();
                     case "2" -> STDOUT.info("Manage favourites Meal\n");
                     case "3" -> STDOUT.info("3 - Configuration\n");
                     case "9" -> System.exit(0);
                     default -> {
-                        STDOUT.info(ANSI_RED + "Please choose correct option\n" + ANSI_RESET);
+                        wrongChoice();
                         mainMenu();
                     }
                 }
             } catch (InputMismatchException ignored) {
-                STDOUT.info(ANSI_RED + "wrong parameter\n" + ANSI_RESET);
+                STDOUT.info(Color.RED.get() + "wrong parameter\n" + Color.RESET.get());
                 STDOUT.info(choice, " :parameter is ");
             }
         }
@@ -68,12 +59,18 @@ public class Menu {
     }
 
     public static void intro() {
-        STDOUT.info(ANSI_BLUE + "\n+-----------------+------+\n");
+        STDOUT.info(Color.BLUE.get() + "\n+-----------------+------+\n");
         STDOUT.info("| WELCOME TO             |\n");
         STDOUT.info("| (®) Meal App Project   |\n");
         STDOUT.info("+-----------------+------+\n");
         STDOUT.info("| Authors:xyz            |\n");
         STDOUT.info("+-----------------+------+\n");
-        STDOUT.info("+-----------------+------+\n\n" + ANSI_RESET);
+        STDOUT.info("+-----------------+------+\n\n" + Color.RESET.get());
+    }
+
+    static void wrongChoice() {
+        STDOUT.info(Color.RED.get() + "\n****************************\n");
+        STDOUT.info("Please choose correct option\n");
+        STDOUT.info("****************************\n" + Color.RESET.get());
     }
 }
