302b86473c92deb47a5dc4e09d8b36a27e332cc9
Maciej <maciejgrochowski93@o2.pl>
2021-05-17 17:58:12 +0200

    updateing displayMealsInMenu(), and changing it's location

--- src/main/java/pl/console/project/menu/Menu.java
+++ src/main/java/pl/console/project/menu/Menu.java
@@ -7,8 +7,11 @@ package pl.console.project.menu;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.console.project.model.ListMeal;
+import pl.console.project.model.Meal;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
@@ -83,4 +86,112 @@ public class Menu {
         }
         scanner.close();
     }
+
+    public void displayMealsInMenu(ListMeal listMeal) {
+        List<Meal> mealsList = listMeal.getListMeals();
+        int pagesAmount = determinePagesAmount(mealsList);
+
+        while (true) {
+            int pageCurrent = 1;
+            for (int i = 1; i <= 7; i++) {
+                int nrOnList = (i + (7 * (pageCurrent - 1)));
+
+                if (mealsList.get(nrOnList) != null) {
+                    System.out.println(i + " - " + mealsList.get(nrOnList).getName());
+                } else {
+                    break;
+                }
+            }
+            System.out.println("Press - 9 - to exit.");
+            changePageOrExit(pageCurrent, pagesAmount);
+        }
+    }
+
+    public int determinePagesAmount(List list) {
+        int size = list.size();
+        int pagesAmount;
+        if (size <= 7) {
+            pagesAmount = 1;
+        } else if (size % 7 == 0) {
+            pagesAmount = (size / 7);
+        } else {
+            pagesAmount = (int) (size / 7) + 1;
+        }
+        return pagesAmount;
+    }
+
+    public void changePageOrExit(int pageCurrent, int pagesAmount) {
+        System.out.println("Press 'e' to check next meals, or 'q' to check previous.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.equals("e") && pageCurrent < pagesAmount) {
+            pageCurrent++;
+        } else if (input.equals("q") && pageCurrent > 1) {
+            pageCurrent--;
+        } else if (input.equals("9")) {
+            exit();
+        }
+    }
+
+    public void exit() {
+        // TODO ????????
+        System.out.println("Exit in progess");
+    }
+
+    //    public void displayMealsInMenu(ListMeal listMeal) throws InterruptedException {
+//        int nr = 1;
+//        int pageCurrent = 1;
+//        List<Meal> listMeals = listMeal.getListMeals();
+//        for (Meal meal : listMeals) {
+//            System.out.println(nr + " - " + listMeals.get(nr + (7 * (pageCurrent - 1))).getName());
+//            nr++;
+//
+//            if (nr == 7 || nr == listMeals.size()) {
+//                if (shouldChangePage(pageCurrent, listMeals.size())) {
+//                    nr = 1;
+//                    Thread.currentThread().start();
+//                }
+//                System.out.println("Press - 9 - to exit.");
+//            }
+//        }
+//    }
+
+//    public boolean shouldDisplayNext() {
+//        System.out.println("Press 'e' to check next meals.");
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//        if (input.equals("e")) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public boolean shouldDisplayPrevious() {
+//        System.out.println("Press 'q' to check previous meals.");
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//        if (input.equals("q")) {
+//            return true;
+//        }
+//        return false;
+//    }
+
+    // 30 % 7 = 2
+
+//    public void displayMealsInMenu() throws InterruptedException {
+//        int i = 1;
+//        int pagesAmount = determinePageAmount(listMeals);
+//        int pageCurrent;
+//        for (Meal meal : listMeals) {
+//            System.out.println(i + " - " + meal.getName());
+//            i++;
+//            if (i == 7) {
+//                i = 1;
+//                Thread.currentThread().wait();
+//                if (shouldDisplayNext()) {
+//                    Thread.currentThread().run();
+//                }
+//            }
+//        }
+//    }
 }
